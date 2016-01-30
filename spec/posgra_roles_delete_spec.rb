bab7d8c8d7affc7288f9c728b8472d13110e3be4
winebarrel <sgwr_dts@yahoo.co.jp>
2016-01-30 21:57:47 +0900

    Add delete role spec

---
+++ spec/posgra_roles_delete_spec.rb
@@ -0,0 +1,77 @@
+describe 'roles (delete)' do
+  include SpecHelper
+
+  subject { export_roles }
+
+  before do
+    apply_roles do
+      <<-RUBY
+        user "alice"
+
+        group "staff" do
+          user "bob"
+        end
+      RUBY
+    end
+  end
+
+  context 'when drop user' do
+    it do
+      apply_roles do
+        <<-RUBY
+          group "staff" do
+            user "bob"
+          end
+        RUBY
+      end
+
+      is_expected.to eq <<-RUBY.unindent.chomp
+        group "staff" do
+          user "bob"
+        end
+      RUBY
+    end
+  end
+
+  context 'when drop user in group' do
+    it do
+      apply_roles do
+        <<-RUBY
+          user "alice"
+          group "staff"
+        RUBY
+      end
+
+      is_expected.to eq <<-RUBY.unindent.chomp
+        user "alice"
+
+        group "staff" do
+          # no users
+        end
+      RUBY
+    end
+  end
+
+  context 'when drop group' do
+    it do
+      apply_roles do
+        <<-RUBY
+          user "alice"
+          user "bob"
+        RUBY
+      end
+
+      is_expected.to eq <<-RUBY.unindent.chomp
+        user "alice"
+        user "bob"
+      RUBY
+    end
+  end
+
+  context 'when drop user and group' do
+    it do
+      apply_roles { '' }
+      is_expected.to be_empty
+    end
+  end
+end
