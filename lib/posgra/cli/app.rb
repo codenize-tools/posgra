885a3a9d497f2dd78b6ac03fe6bdbac109e9bf0f
Antoine Augusti <antoine@drivy.com>
2016-11-21 09:49:09 +0100

    Give the possibility to use env variables for a default connection

--- lib/posgra/cli/app.rb
+++ lib/posgra/cli/app.rb
@@ -1,10 +1,10 @@
 class Posgra::CLI::App < Thor
-  class_option :host, :default => 'localhost', :aliases => '-h'
-  class_option :port, :type => :numeric, :default => 5432, :aliases => '-p'
-  class_option :dbname, :default => 'postgres', :aliases => '-d'
-  class_option :user, :aliases => '-U'
-  class_option :password, :aliases => '-P'
-  class_option :'account-output', :default => 'account.csv'
+  class_option :host, :default => ENV['POSGRA_DB_HOST'] || 'localhost', :aliases => '-h'
+  class_option :port, :type => :numeric, :default => ENV['POSGRA_DB_PORT'] || 5432, :aliases => '-p'
+  class_option :dbname, :default => ENV['POSGRA_DB_DATABASE'] || 'postgres', :aliases => '-d'
+  class_option :user, :default => ENV['POSGRA_DB_USER'], :aliases => '-U'
+  class_option :password, :default => ENV['POSGRA_DB_PASSWORD'], :aliases => '-P'
+  class_option :'account-output', :default => ENV['POSGRA_ACCOUNT_FILEPATH'] || 'account.csv'
   class_option :color, :type => :boolean, :default => true
   class_option :debug, :type => :boolean, :default => false
 
