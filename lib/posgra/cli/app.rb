bb0c79d35080490edd30aaeac78d7864d1091bdc
Antoine Augusti <antoine@drivy.com>
2016-12-01 14:48:46 +0100

    Cast port to int

--- lib/posgra/cli/app.rb
+++ lib/posgra/cli/app.rb
@@ -1,6 +1,6 @@
 class Posgra::CLI::App < Thor
   class_option :host, :default => ENV['POSGRA_DB_HOST'] || 'localhost', :aliases => '-h'
-  class_option :port, :type => :numeric, :default => ENV['POSGRA_DB_PORT'] || 5432, :aliases => '-p'
+  class_option :port, :type => :numeric, :default => ENV['POSGRA_DB_PORT'].to_i || 5432, :aliases => '-p'
   class_option :dbname, :default => ENV['POSGRA_DB_DATABASE'] || 'postgres', :aliases => '-d'
   class_option :user, :default => ENV['POSGRA_DB_USER'], :aliases => '-U'
   class_option :password, :default => ENV['POSGRA_DB_PASSWORD'], :aliases => '-P'
